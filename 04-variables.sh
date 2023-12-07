a06784e9bb119aebe7c28e2a5fe6b4a951e57868
manofcloud <izzyschoolofdevops@gmail.com>
2023-12-06 21:18:15 -0800

    AutoPush

---
+++ 04-variables.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# what is a variable?
+# a variable is something that holds the data
+
+
+# the Advantage and diosadvantages in shell is that there are no data types 
+
+a=10
+b=abc
+# in bash, everything will be considered as a string by default
+
+# how can we print a variable?
+
+echo printing the value of a $abcecho printing the value of a ${a}
