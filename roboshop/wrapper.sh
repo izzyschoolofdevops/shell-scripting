e87665a073f80fcadd7302048ab15e30a74169fb
manofcloud <izzyschoolofdevops@gmail.com>
2023-12-11 14:17:02 -0800

    AutoPush

--- roboshop/wrapper.sh
+++ roboshop/wrapper.sh
@@ -4,6 +4,6 @@ bash components/$1.sh
 
 if [ $? -ne 0 ] ; then 
 
-   echo -e "\e31m example usage: \e[0m bash wrapper.sh componentName"
-   exit 30
-   fi
+    echo -e "\e31m example usage: \e[0m bash wrapper.sh componentName"
+    exit 30
+fi
