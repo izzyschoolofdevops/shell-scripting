be414002a83e2da506560c6d8934b7af0cdc4af1
manofcloud <izzyschoolofdevops@gmail.com>
2023-12-11 12:27:19 -0800

    AutoPush

--- ifelse.sh
+++ ifelse.sh
@@ -3,9 +3,9 @@
 echo -e "Demo on If , If Else If usage"
 ACTION=$1
 
-if [ $ACTION == start ] ; then 
+if [ $ACTION==start ] ; then 
     echo -e "\e[32m starting shipping service \e[0m"
     exit 0
 
-    fi
+fi
  
