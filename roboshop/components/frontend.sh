8ec9d97e27e0d0f487c662dc1969064183a23ad5
manofcloud <izzyschoolofdevops@gmail.com>
2023-12-12 09:30:11 -0800

    AutoPush

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -2,6 +2,16 @@
 
 echo -e "***** \e[35m configuring frontend \e[0m *****"
 
+
+
+echo -n "Downloading Component $1 :"
+curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
+if [ $? -eq 0 ] ; then 
+    echo -e "\e[33m success \e[0m"
+else 
+    echo -e "\e[31m Failure \e[0m"
+fi
+
 echo "Installing Nginx"
 # yum install nginx -y
 # systemctl enable nginx
