ef64330b0ea9738759dd99d383929ff76685b5c4
manofcloud <izzyschoolofdevops@gmail.com>
2023-12-12 11:14:17 -0800

    AutoPush

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -5,6 +5,8 @@ echo -e "***** \e[32m Confioguring frontend \e[0m*****"
 echo "Installing Nginx ;"
 yum install nginx -y
 
+yum install wget -y 
+
 #systemctl enable nginx
 #systemctl start nginx
 #curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
