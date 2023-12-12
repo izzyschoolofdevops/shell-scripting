e0b4589f7f93bedf45f740cead7c50a9c8bc0797
manofcloud <izzyschoolofdevops@gmail.com>
2023-12-12 10:57:09 -0800

    AutoPush

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
 
-# yum install nginx -y
+UID=$(id -u)
+COMPONENT=$1
+if [ $UID -ne 0 ] ; then 
+    echo -e "\e[32m This script is expected to be excuted with sudo or as a root user \e[0m"
+    echo -e "\e[35m Example Usage: \n\t\t \e[0m sudo bash scriptName componentName"
+    exit 1 
+fi
+
+echo -e "***** \e[35m configuring frontend \e[0m *****"
+
+echo  "Installing Nginx :"
+yum install nginx  -y       &>> /tmp/frontend.log
+if [ $? -eq 0 ] ; then 
+    echo -e "\e[32m success \e[0m"
+else 
+    echo -e "\e[32m Failure \e[0m"
+
+fi
+
 # systemctl enable nginx
 # systemctl start nginx
 # curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
